d8715215d5f43d3bdf48488a260190ec727b2cbc
Yeffry Diaz <yeffrydiaz@gmail.com>
2018-01-14 12:37:52 -0500

    logic

--- designs.js
+++ designs.js
@@ -1,10 +1,29 @@
-// Select color input
-// Select size input
-
+$(document).ready(makeGrid);
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(()=>{
+  event.preventDefault();
+  makeGrid();
+});
 
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid() {//alert();
+  $('#pixel_canvas').html('');
+  const gridHeight = $('#input_height').val(),
+         gridWidth = $('#input_width').val();
+  for (let x = 1; x <= gridWidth; x++) {  
+    $('#pixel_canvas')
+      .append(`<tr id="${x}">${gridHeight}</tr>`);      
+    for (let y = 1; y <= gridHeight; y++){       
+      $('#'+x)
+      .append(`<td id="${x}${y}"></td>`)
+    }
+  }
 }
+
+$(document).on('click', '#pixel_canvas',function (e) {
+  const id = $('#pixel_canvas').children().attr('id');
+  const colorPicked = $('#colorPicker').val();
+  let cellColor = e.target.style.backgroundColor;
+ // $('#sizePicker').prepend(typeof cellColor);
+  e.target.style.backgroundColor = 
+    cellColor==='' ? colorPicked  : ''; 
+  })
