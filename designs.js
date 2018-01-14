f6a20629496d8e7b9f5c29d3dead392b97b0d6a3
Yeffry Diaz <yeffrydiaz@gmail.com>
2018-01-14 14:10:58 -0500

    some css and other logic

--- designs.js
+++ designs.js
@@ -1,29 +1,43 @@
 $(document).ready(makeGrid);
-// When size is submitted by the user, call makeGrid()
+
 $('#sizePicker').submit(()=>{
   event.preventDefault();
   makeGrid();
 });
 
-function makeGrid() {//alert();
+function makeGrid() {
   $('#pixel_canvas').html('');
   const gridHeight = $('#input_height').val(),
          gridWidth = $('#input_width').val();
+             pSize = $('#pixelsize').val();
   for (let x = 1; x <= gridWidth; x++) {  
     $('#pixel_canvas')
-      .append(`<tr id="${x}">${gridHeight}</tr>`);      
+      .append(`<tr id="row${x}" class="rows">${gridHeight}</tr>`);      
     for (let y = 1; y <= gridHeight; y++){       
-      $('#'+x)
-      .append(`<td id="${x}${y}"></td>`)
+      $('#row'+x)
+        .append(`<td id="${x}${y}" class="columns"></td>`)
     }
   }
+  pixelSize(pSize);
 }
 
-$(document).on('click', '#pixel_canvas',function (e) {
-  const id = $('#pixel_canvas').children().attr('id');
-  const colorPicked = $('#colorPicker').val();
-  let cellColor = e.target.style.backgroundColor;
- // $('#sizePicker').prepend(typeof cellColor);
-  e.target.style.backgroundColor = 
-    cellColor==='' ? colorPicked  : ''; 
+$(document)
+ .on('keyup', '#pixelsize', e => {
+   const pSize = $('#pixelsize').val();
+   pixelSize(pSize);
+})
+
+function pixelSize(x){
+  $('.rows, .columns')
+    .css({
+      'width': x,
+      'height': x
+    })
+}
+$(document).on('click', '#pixel_canvas', e => {
+  const id = $('#pixel_canvas').children().attr('id'),
+        colorPicked = $('#colorPicker').val(),
+        cellColor = e.target.style.backgroundColor;
+     e.target.style.backgroundColor = 
+      cellColor==='' ? colorPicked  : ''; 
   })
